2dbfa95fc3da3a8a9d175556f2ee8900acba5676
Parimal <56225634+parimalmahindrakar@users.noreply.github.com>
2020-12-31 08:40:53 +0530

    Update views.py

--- PostalCodesApp/views.py
+++ PostalCodesApp/views.py
@@ -11,6 +11,7 @@ def getCode(request,code_):
     for i in code_:
         data['data'].update({i.area:i.code,'city':i.city,'state':i.state,"is_available":True})
     if len(code_) == 0:
-        return JsonResponse({"is_available":False})
+        data['data'].update({"is_available": False})
+        return JsonResponse(data)
     else:
         return JsonResponse(data)
